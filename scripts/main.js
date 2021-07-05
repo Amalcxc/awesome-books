4d8902d151304d49d8fcdcf018ebe2c5215a2445
Amalcxc <amalcxirsi@gmail.com>
2021-07-05 22:02:10 +0300

    add books list functions

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,28 @@ class Book {
   }
 }
 
-const books = [];
+let books = [];
 
 const form = document.getElementById('form');
 
+
+if ('booksData' in localStorage) {
+  books = JSON.parse(localStorage.booksData);
+}
+
 function addBook(event) {
   books.push(new Book(form[0].value, form[1].value));
-  event.preventDefault();
+  localStorage.setItem('booksData', JSON.stringify(books));
+
 }
 
 form.addEventListener('submit', addBook);
 
+const bookList = document.getElementById('listed-books');
 
+for(let i = 0; i < books.length; i+=1){
+  let bookTitle = document.createElement('p');
+  bookTitle.textContent = books[i].title;
+  bookList.appendChild(bookTitle);
+}
 
